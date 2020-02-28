b674efde133ef81ac100e455dd7268811b83a53e
MichaelRempe <michaeljrempe@gmail.com>
2020-02-27 20:04:48 -0500

    exercies model constructed to reflect capture data

--- app/models/exerciseModel.js
+++ app/models/exerciseModel.js
@@ -4,13 +4,9 @@ const Schema = mongoose.Schema;
 const ExerciseSchema = new Schema({
   type: {
     type: String,
-    trim: true,
-    required: "Enter an exercise type"
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter an exercise name"
+    enum: ["Bench Press", "DB Bench", "Pec Dec", "Incline Bench", "Military Press", "Arnold Press",
+    "Lateral Flys", "Rear-delt Flys", "Yates Row", "DB Row", "Weighed Pull-ups", "Shrugs", "Lat Pull",
+    "Low-Bar Squat", "Lunges", "Quad Extensions", "Calve Raises", "Deadlift", "Back Extension", "Hamstring Curls", "Band Extensions", "Bicep Curl","Hammer Curl", "Barbel Curl", "Tricep Extensions", "Weighted Dips", "Skull Crushers" ]
   }
 });
 
